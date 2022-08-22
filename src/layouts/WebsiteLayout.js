8c7fea61d07b1adb596210ddfcecac8024471923
LautiAlessa <alessandrolautaror@gmail.com>
2022-08-22 16:28:00 -0300

    added Cities.js to WebsiteLayout

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,7 +1,7 @@
 import Welcome from '../pages/Welcome';
 import Footer from '../components/Footer';
 import Carousel from '../components/Carousel';
-import Cities from '../components/Cities';
+import Cities from '../pages/Cities';
 
 export default function WebsiteLayout(props) {
     return (
