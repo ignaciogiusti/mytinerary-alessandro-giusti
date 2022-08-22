02cc38fff9de28a8e03b1c52a18c2da6c1d2d81a
LautiAlessa <alessandrolautaror@gmail.com>
2022-08-22 16:31:45 -0300

    created Cities page and styles

---
+++ src/pages/Cities.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '../styles/Cities.css'
+
+export default function Cities() {
+
+
+    const cities = [
+        { url: "/img/buenosAires.jpg", city: "Buenos Aires", country: "Argentina" },
+        { url: "/img/venice.jpg", city: "Venice", country: "Italy" },
+        { url: "/img/cancun.jpg", city: "Cancun", country: "Mexico" },
+        { url: "/img/capetown.jpg", city: "Cape Town", country: "South Africa" },
+        { url: "/img/edinburgh.jpg", city: "Edinburgh", country: "Scotland" },
+        { url: "/img/helsinki.jpg", city: "Helsinki", country: "Finland" },
+        { url: "/img/madrid.jpg", city: "Madrid", country: "Spain" },
+        { url: "/img/milan.jpg", city: "Milan", country: "Italy" },
+        { url: "/img/paris.jpg", city: "Paris", country: "France" },
+        { url: "/img/RioDJ.png", city: "Rio De Janeiro", country: "Brasil" },
+        { url: "/img/Shanghai.jpg", city: "Shanghai", country: "China" },
+        { url: "/img/Zermatt-Switzerland.jpg", city: "Zermatt", country: "Switzerland" },
+    ];
+    
+        const imgCitiesContainerView = (itemsMap) => (
+        <div className='City-container'>
+            <img className="City-img" src={itemsMap.url} />
+            <h3 className='City-text text-center'>{itemsMap.city} - {itemsMap.country}</h3>
+        </div>
+        )
+
+
+
+    return (
+        <div>
+                <div className='citiesPageContainer'>
+                    { 
+                        cities.map(imgCitiesContainerView) 
+                    }
+                </div>
+        </div>
+    )
+}
