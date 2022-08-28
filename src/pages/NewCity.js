a659e031e331afb14b5e2a3e883c558670e283a8
ignaciogiusti <nachogiusti94@gmail.com>
2022-08-28 01:22:16 -0300

    added a form tag, changed class names to more fitting names

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,13 +1,13 @@
 import InputNewCity from "../components/InputNewCity";
-import '../styles/NewCity.css'
-
+import '../styles/NewCity.css';
 
 export default function NewCity() {
   return (
-    <div className="Input-NewCity">
-      <h1 className="text-light">Add your New City ¡HERE!</h1>
-      <InputNewCity/>
-      <button className="button-NewCity">Send</button>
+    <div className="Main-NewCity">
+      <form className="Form-NewCity">
+        <h1 className="text-light">Add a New City ¡HERE!</h1>
+        <InputNewCity />
+      </form>
     </div>
   )
 }
