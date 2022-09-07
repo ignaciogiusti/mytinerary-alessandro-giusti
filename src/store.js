69bc802cdf996f9dff1427b891accb3fbbf043f6
LautiAlessa <alessandrolautaror@gmail.com>
2022-09-06 21:55:00 -0300

    fixed

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import citiesSlice from './features/citiesSlice'
+import citiesAPI from './features/citiesAPI'
 
 //instancia del store
 export default configureStore ({
     reducer: {
-        cities: citiesSlice 
+        cities: citiesSlice,
+        [citiesAPI.reducerPath]: citiesAPI.reducer
     },
 })
