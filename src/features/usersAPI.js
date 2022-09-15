ca9b14dcf27de45f33a9eb09edd6d7ec69e32d74
LautiAlessa <alessandrolautaror@gmail.com>
2022-09-15 13:15:28 -0300

    created feature usersAPI

---
+++ src/features/usersAPI.js
@@ -0,0 +1,26 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const usersAPI = createApi({
+    reducerPath: "usersAPI",
+
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:4000/"
+    }),
+
+    // recibe search como parametro, que viene desde Cities.js cuando se llama al useGetAllCitiesQuery
+    // en el ternario, pregunta; si search es negado, es decir, no existe (sea undefined o lo que sea), va a mandar por query un string vacio ("")
+    // en caso de que si exista search, va a usar search, osea, lo que le está llegando
+    endpoints: (builder) => ({
+        createUser: builder.query({
+            query: (user) => ({
+                url: '/auth/signup/', 
+                method: 'POST',
+                body: user
+            })
+        })
+    }),
+})
+
+export default usersAPI
+
+export const {useCreateUserQuery} = usersAPI
