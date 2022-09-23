f9989d2e78b70c3639f7edfdf69a3b74ca74c880
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-23 18:56:49 -0300

    Created Profile.js page and styles page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,9 +2,12 @@ import React from 'react'
 import '../styles/Profile.css'
 
 export default function Profile() {
-  return (
-    <div className='Main-BG'>
-        <h2 className='text-center text-light'>User <span className='Title-Tinerary'>Profile</span></h2>
-    </div>
-  )
+    return (
+        <>
+            <div className='Main-BG'>
+                <h2 className='text-center text-light'>User <span className='Title-Tinerary'>Profile</span></h2>
+
+            </div>
+        </>
+    )
 }
