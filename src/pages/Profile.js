0a340a84b99db9763b31d15876b21143e7d54bae
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-24 10:05:30 -0300

    created functions to capture user info that will be modified

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import '../styles/Profile.css'
+import axios from "axios";
+import {toast} from 'react-hot-toast'
+import { useEffect, useState } from 'react'
 
-export default function Profile() {
+export default function Profile({user}) {
     const userName = JSON.parse(localStorage.getItem('user'))?.name
     const userPhoto = JSON.parse(localStorage.getItem('user'))?.photo
+
+    const [editUser, setEditUser] = useState({
+        name: '',
+        lastName: '',
+        photo: '',
+        country: ''
+      })
+      const inputHandler = (e) => {
+        setEditUser({
+          ...editUser, //los 3 puntos hace que se guarde el dato que se actualizo y no se borre (spread operator)
+          [e.target.name]: e.target.value
+        })
+      }
+      const userEditSubmit = async () => {
+        try {
+            if (Object.values(editUser).some((value) => !value)) {
+                return toast.error('You need to complete a field', {position: "bottom-right"})
+            }
+            const response = await axios.patch(`http://localhost:4000/auth/profile/${user._id}`, editUser)
+            console.log(response);
+            if (response.data.success) {
+              toast.success(`Your information was modified!`, {position: "bottom-right"})
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <>
@@ -14,14 +44,14 @@ export default function Profile() {
                 </div>
                 <form className='Profile-Form'>
                     <label className='text-light col text-indent'>First Name:
-                        <input type='text' name='name' className='Input-NewCity flex-center' /* onChange={inputHandler} */ ></input></label>
+                        <input type='text' name='name' className='Input-NewCity flex-center' onChange={inputHandler} ></input></label>
                     <label className='text-light col text-indent'>Last Name:
-                        <input type='text' name='name' className='Input-NewCity flex-center' /* onChange={inputHandler} */ ></input></label>
+                        <input type='text' name='lastName' className='Input-NewCity flex-center' onChange={inputHandler} ></input></label>
                     <label className='text-light col text-indent'>Photo URL:
-                        <input type='text' name='name' className='Input-NewCity flex-center' /* onChange={inputHandler} */ ></input></label>
+                        <input type='text' name='photo' className='Input-NewCity flex-center' onChange={inputHandler} ></input></label>
                     <label className='text-light col text-indent'>Country:
-                        <input type='text' name='name' className='Input-NewCity flex-center' /* onChange={inputHandler} */ ></input></label>
-                    <button className='button-NewCity flex-center' /* onClick={editCity} */ >Send</button>
+                        <input type='text' name='country' className='Input-NewCity flex-center' onChange={inputHandler} ></input></label>
+                    <button className='button-NewCity flex-center' onClick={userEditSubmit} >Send</button>
                 </form>
             </div>
         </>
