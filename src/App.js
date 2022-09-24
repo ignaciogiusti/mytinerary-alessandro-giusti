05d96694f3bbc3badf7c9a88f363dcbd55c45341
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-24 20:46:02 -0300

    removed .replace method

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   axios.interceptors.request.use(
     (config) => {
-      const token = localStorage.getItem("token").replace(/["']/g, "")
+      const token = localStorage.getItem("token")
       console.log(token)
       if (token) {
         config.headers["Authorization"] = "Bearer " + token;
